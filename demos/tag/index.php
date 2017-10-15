492e47d805561ebd25697e9a211f600e56485942
Andrew Zito <azito122@gmail.com>
2017-10-15 17:12:15 -0400

    Word tagging: initial commit

---
+++ demos/tag/index.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../../style/tag/main.css">
+    <script src="../../module_settings/settings.js"></script>
+    <script
+      src="https://code.jquery.com/jquery-3.2.1.min.js"
+      integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+      crossorigin="anonymous"></script>
+    <script src="https://d3js.org/d3.v4.js"></script>
+    <script src="../../demos/tag/main.js"></script>
+  </head>
+
+  <body>
+    <div class="sidebar">
+      <div class="settings-menu">
+      </div>
+    </div>
+    <div class="main">
+    </div>
+  </body>
+
+</html>
